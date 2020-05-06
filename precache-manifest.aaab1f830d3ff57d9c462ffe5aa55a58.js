6f7ac2a184e6ff29a1a2cfc0629c45ac629c4b13
Rodrigo Silveira <r3850365@gmail.com>
2020-05-06 10:53:29 -0700

    gh-pages  -  Updates

---
+++ precache-manifest.aaab1f830d3ff57d9c462ffe5aa55a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f1620550e8f5ef39bad7c1023c89dc",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "78a93a7e2c307ce0d0d6",
+    "url": "/react_todo/static/css/main.4d192a29.chunk.css"
+  },
+  {
+    "revision": "e3169aa4931a37c50476",
+    "url": "/react_todo/static/js/2.cba5f499.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_todo/static/js/2.cba5f499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a93a7e2c307ce0d0d6",
+    "url": "/react_todo/static/js/main.c131df2a.chunk.js"
+  },
+  {
+    "revision": "2e813195e79d72ed3412",
+    "url": "/react_todo/static/js/runtime-main.e59266dc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e006ecaa0a807bb76005f38bfbe8b90.js"
+  "/react_todo/precache-manifest.aaab1f830d3ff57d9c462ffe5aa55a58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
